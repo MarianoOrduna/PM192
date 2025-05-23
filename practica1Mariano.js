9670ff00443aa3cbe562d4fb785e98001470748d
MarianoOrduna <122043964@upq.edu.mx>
2025-05-22 18:41:25 -0600

    Ejercicio B p1

--- practica1Mariano.js
+++ practica1Mariano.js
@@ -6,3 +6,6 @@ nombre = "Cinthia Lizeth";
 const saludo = "Hola" + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
 
+const cuadrado = (numero) => numero * numero;
+console.log(cuadrado(2));
+
