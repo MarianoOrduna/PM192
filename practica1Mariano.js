f32b169b135983e861c79300e5b07a30f76da1ca
MarianoOrduna <122043964@upq.edu.mx>
2025-05-22 18:41:39 -0600

    Ejercicio C p1

--- practica1Mariano.js
+++ practica1Mariano.js
@@ -9,3 +9,5 @@ console.log(saludo);
 const cuadrado = (numero) => numero * numero;
 console.log(cuadrado(2));
 
+const saludoPersonalizado = (nombre, edad) => 'Hola, me llamo ${nombre} y tengo ${edad} años.';
+console.log(saludoPersonalizado("Mariano", 21));
