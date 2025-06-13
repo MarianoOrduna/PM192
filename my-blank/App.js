faaae907135a15381ef45ee49b232ca37c9ee9dc
MarianoOrduna <122043964@upq.edu.mx>
2025-06-12 18:36:15 -0600

    Practica 6PM

--- my-blank/App.js
+++ my-blank/App.js
@@ -2,21 +2,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import React,{useState} from 'react';
-const Texto=()=>{
+
+const Texto=({style})=>{
 
   const[contenido,setContenido]=useState('Hola Mariano React')
   const actualizarTexto=()=>{setContenido('Estado Actualizado')
   }
-  return(<Text onPress={actualizarTexto}>{contenido}</Text>)
+  return(<Text style={[styles.Text,style]} onPress={actualizarTexto}>{contenido}</Text>)
 }
 
 /* Zona 2 Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto >  </Texto>
-      <Texto >  </Texto>
-      <Texto > </Texto>
+      <Texto  style={styles.red}> </Texto>
+      <Texto  style={styles.blue} ></Texto>
+      <Texto  style={styles.green}></Texto>
       <Button title="Presioname"> </Button>
       <StatusBar style="auto" />
     </View>
@@ -27,7 +28,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
+    flexDirection:'column',
   },
+  Text:{
+    color:'black',
+    fontSize:25,
+/*     height:100,
+ */    /* width:100, */
+  },
+  red: { /* flex:1, */ backgroundColor: 'red'},
+  green: {/* flex:2, */ backgroundColor: 'green'},
+  blue: { /* flex:3, */backgroundColor: 'blue'}, 
+
 });
