8f30db8019f2ee5c8379459c97d40d09d1a18b47
MarianoOrduna <122043964@upq.edu.mx>
2025-06-18 19:01:15 -0600

    Update App.js

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,95 +1,45 @@
 /* Zona 1 importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Switch} from 'react-native';
-import { SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React,{useState} from 'react';
 
+const Texto=({style})=>{
+
+  const[contenido,setContenido]=useState('Hola Mariano React')
+  const actualizarTexto=()=>{setContenido('Estado Actualizado')
+  }
+  return(<Text style={[styles.Text,style]} onPress={actualizarTexto}>{contenido}</Text>)
+}
 
 /* Zona 2 Main */
 export default function App() {
-const [activarSwitch, setactivarSwitch]=useState(false);
-const[modoOscuro,setModoOscuro]=useState(false);
-
   return (
-    <SafeAreaProvider>
-
-    
-    <SafeAreaView style={[styles.contenedor, modoOscuro && styles.fondoOscuro]}>
-
-{/* Aqui van los componentes */}
-
-<Text style={[styles.titulo,modoOscuro && styles.textoClaro]}>
-practica swith
-</Text>
-
-<View style={styles.opcion}> 
-<Text style={[styles.etiqueta,modoOscuro && styles.textoClaro]}> 
-  Activar Switch2
-</Text>
-<Switch value={activarSwitch}
-onValueChange={setactivarSwitch}
-trackColor={{false:'#ccc',true:"#4caf50"}}
-thumbColor={activarSwitch? '#fffff':'#999999'}>
-</Switch>
-</View>
-
-<View style={styles.opcion}> 
-<Text style={[styles.etiqueta,modoOscuro && styles.textoClaro]}> 
-  modoOscuro
-</Text>
-<Switch value={modoOscuro}
-onValueChange={setModoOscuro}
-disabled={activarSwitch}
-trackColor={activarSwitch?
-  {false:'#ff9999',true:'#ff3b30'}
-  :{false:'ccc',true:'#4caf50'}
-}
-thumbColor={
-  activarSwitch? '#ff3b30'
-  :modoOscuro
-  ?'#fffff'
-  :'#999999'
-}
->
-</Switch>
-</View>
-
-    </SafeAreaView>
-  </SafeAreaProvider>
-    
-
-    
+    <View style={styles.container}>
+      <Texto  style={styles.red}> </Texto>
+      <Texto  style={styles.blue} ></Texto>
+      <Texto  style={styles.green}></Texto>
+      <Button title="Presioname"> </Button>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 /*  zona 3 estilos */
 const styles = StyleSheet.create({
-  contenedor:{
-    flex:1,
-    background:"#fff",
-    paddingHorizontal:30,
-    justifyContent:"center"
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'base-line',
+    justifyContent: 'center',
+    flexDirection:'column',
   },
-  titulo:{
-    fontSize:24,
-    marginBottom:40,
-    textAlign:"center",
-    fontWeight:'bold'
+  Text:{
+    color:'black',
+    fontSize:25,
+/*     height:100,
+ */    /* width:100, */
   },
-  fondoOscuro:{
-    backgroundColor:'black',
-  },
-  textoClaro:{
-    color:'white'
-  },
-  opcion:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    marginBottom:30,
-    alignItems:'center'
-  },
-  etiqueta:{
-    fontSize:18,
-
-  }
+  red: { /* flex:1, */ backgroundColor: 'red'},
+  green: {/* flex:2, */ backgroundColor: 'green'},
+  blue: { /* flex:3, */backgroundColor: 'blue'}, 
 
 });
